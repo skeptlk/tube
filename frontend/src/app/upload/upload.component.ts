8422d7b3b21e8015f998e9248c7f084139145daa
sceptlc <sceptlc@bk.ru>
2020-12-26 02:13:49 +0300

    upload drag-n-drop

--- frontend/src/app/upload/upload.component.ts
+++ frontend/src/app/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService, VideoService } from '../services';
@@ -9,15 +9,25 @@ import { AuthService, VideoService } from '../services';
     styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent {
-    
+
+    @ViewChild("fileInp") fileInput: ElementRef;
+
+    @HostListener('window:dragover', ['$event'])
+    dragover(e) { e.preventDefault(); }
+
+    @HostListener('window:drop', ['$event'])
+    drop(e) { e.preventDefault(); }
+
+
     video: File = null;
     uploadForm = new FormGroup({
         title: new FormControl('', [Validators.required]),
         description: new FormControl('')
     })
+    dragActive: boolean = false;
 
     constructor(
-        private vidService: VideoService, 
+        private vidService: VideoService,
         private auth: AuthService,
         private router: Router
     ) { }
@@ -27,22 +37,34 @@ export class UploadComponent {
             this.video = files.item(0);
     }
 
+    selectFile() {
+        this.fileInput.nativeElement.click();
+    }
+
+    dropHandler(e) {
+        if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+            let item = e.dataTransfer.items[0];
+            if (item.kind === 'file')
+                this.video = item.getAsFile();
+        }
+    }
+
     submit(e) {
         e.preventDefault();
-        
+
         if (this.uploadForm.valid && this.video) {
-    
+
             var fd = new FormData();
             fd.append('video', this.video, this.video.name);
             fd.append('title', this.uploadForm.get('title').value);
             fd.append('description', this.uploadForm.get('description').value);
             fd.append('userID', this.auth.currentUserValue.id?.toString());
-            
+
             this.vidService.upload(fd)
                 .subscribe((resp) => {
                     console.log(resp)
                     this.router.navigate([this.vidService.BASE_URL, 'v', resp.id])
-                } );
+                });
         }
     }
 }
